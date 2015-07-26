f22ebc1350a9820a38c016172b43700f9c499f9a
rashmirathi <jasbirsingh279@gmail.com>
2015-07-26 02:03:45 +0100

    adds support for escape chars

--- parser.hs
+++ parser.hs
@@ -1,17 +1,52 @@
 import System.Environment
+import Control.Monad
 import Text.ParserCombinators.Parsec hiding (spaces)
 
-symbol :: Parser Char
-symbol = oneOf "!$%&|*+-/:<=?>@i^_~#  "
-
-readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input  of
-                   Left err -> "No match :" ++ (show err)
-                   Right val -> "Found value"
+data LispVal = Atom String
+            | List [LispVal]
+            | DottedList [LispVal] LispVal
+            | Number Integer
+            | String String
+            | Bool Bool
 
 spaces :: Parser ()
 spaces = skipMany1 space
 
+symbol :: Parser Char
+symbol = oneOf "!$%&|*+-/:<=?>@i^_~#"
+
+escapeChars = do
+    char '\\'
+    oneOf ['"', '\\', 'n', 'r', 't']
+
+parseString :: Parser LispVal
+parseString = do
+            char '"'
+            x <- many (escapeChars <|> noneOf "\"")
+            char '"'
+            return (String x)
+
+parseAtom :: Parser LispVal
+parseAtom = do first <- letter <|> symbol
+               rest <- many (letter <|> digit <|> symbol)
+               let atom = [first] ++ rest
+               return $ case atom of
+                    "#t" -> Bool True
+                    "#f" -> Bool False
+                    otherwise -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom <|> parseString <|> parseNumber
+
+readExpr :: String -> String
+readExpr input = case parse (parseExpr) "lisp" input  of
+                   Left err -> "No match: " ++ (show err)
+                   Right val -> "Found value"
+
 main :: IO()
 main = do args <- getArgs
           putStrLn . readExpr $ args !! 0
+
