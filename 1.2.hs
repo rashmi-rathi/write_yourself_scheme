f9ac160b08197a96660707ad199149353fdbebc0
Jasbir Singh <jasbirsingh279@gmail.com>
2015-08-25 20:30:26 +0100

    remove files

--- 1.2.hs
+++
@@ -1,6 +0,0 @@
-import System.Environment
-
-
-main = do
-    args <- getArgs
-    putStrLn( "Hello , " ++ args !! 0 ++  " and "++ args !! 1)
--- 1.3.hs
+++
@@ -1,7 +0,0 @@
-import System.Environment
-
-main = do
-    args <- getArgs
-    let x = read (args !! 0)::Int
-        y = read (args !! 1)::Int
-        in putStrLn . show  $ (x + y)
