de85a9d1f405f3fcd6fac91937a7fc15c163c003
Jasbir Singh <jasbirsingh279@gmail.com>
2015-09-21 20:41:19 +0100

    Add boolean operators

--- evaluator.hs
+++ evaluator.hs
@@ -255,22 +255,54 @@ primitives = [("+", numericBinop (+)),
              ("/", numericBinop div),
              ("mod", numericBinop mod),
              ("quotient", numericBinop quot),
-             ("remainder", numericBinop rem)]
+             ("remainder", numericBinop rem),
+             ("=", numBoolBinop (==)),
+             ("<", numBoolBinop (<)),
+             (">", numBoolBinop (>)),
+             (">=", numBoolBinop (>=)),
+             ("<=", numBoolBinop (<=)),
+             ("&&", boolBoolBinop (&&)),
+             ("||", boolBoolBinop (||)),
+             ("string=?", strBoolBinop(==)),
+             ("string<?", strBoolBinop (<)),
+             ("string>?", strBoolBinop (>)),
+             ("string<=?", strBoolBinop (<=)),
+             ("string>=?", strBoolBinop (>=))]
+
+boolBinop :: (LispVal -> ThrowsError a) -> (a -> a -> Bool) -> [LispVal] -> ThrowsError LispVal
+boolBinop unpacker op args  = if length args /= 2
+                                then throwError $ NumArgs 2 args
+                                else do left <- unpacker $ args !! 0
+                                        right <- unpacker $ args !! 1
+                                        return $ Bool $ left `op` right
 
 numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError LispVal
 numericBinop op [] = throwError $ NumArgs 2 []
 numericBinop op singleVal@[_] = throwError $ NumArgs 2 singleVal
 numericBinop op params = mapM unpackNum params >>= return . Number . foldl1 op
 
+numBoolBinop = boolBinop unpackNum
+strBoolBinop = boolBinop unpackStr
+boolBoolBinop = boolBinop unpackBool
+
 unpackNum :: LispVal -> ThrowsError Integer
 unpackNum (Number n) = return n
 {-unpackNum (String n) = let parsed = reads n in-}
-  {-if null parsed-}
-    {-then throwError $ TypeMismatch "number" $ String n-}
-    {-else fst $ parsed !! 0-}
-
+{-if null parsed-}
+{-then throwError $ TypeMismatch "number" $ String n-}
+{-else fst $ parsed !! 0-}
 unpackNum (List [n]) = unpackNum n
 unpackNum notNum = throwError $ TypeMismatch "number" notNum
+
+unpackStr :: LispVal -> ThrowsError String
+unpackStr (String s) = return s
+unpackStr (Number s) = return . show $ s
+unpackStr (Bool s) = return . show $ s
+unpackStr notStr = throwError $ TypeMismatch "string" notStr
+
+unpackBool :: LispVal -> ThrowsError Bool
+unpackBool (Bool b) = return b
+unpackBool notBool = throwError $ TypeMismatch "bool" notBool
 
 evaluator :: String -> LispVal
 evaluator = extractValue . eval . extractValue . readExpr
