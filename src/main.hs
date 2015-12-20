92b1a1b961bc933f7326e9c1fc12ec4249b85aaa
Jasbir Singh <jasbirsingh279@gmail.com>
2015-12-20 20:19:14 +0000

    Add a basic repl
    Chapter 5 from Tang's book

--- src/main.hs
+++ src/main.hs
@@ -7,6 +7,7 @@ import Data.Complex
 import Data.Array
 import Control.Monad
 import Control.Monad.Error
+import System.IO
 import Text.ParserCombinators.Parsec hiding (spaces)
 import Numeric
 
@@ -466,12 +467,36 @@ cond args =
 evaluator :: String -> LispVal
 evaluator = extractValue . join . fmap eval . readExpr
 
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+  result <- prompt
+  if pred result
+    then return ()
+    else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>> ") evalAndPrint
+
 main :: IO ()
 main =
   do
     args <- getArgs
-    evaled <- return $ liftM show $ readExpr (args !! 0) >>= eval
-    putStrLn $ extractValue $ trapError evaled
+    case length args of
+      0 -> runRepl
+      1 -> evalAndPrint $ args !! 0
+      otherwise -> putStrLn "Program takes only 0 or 1 argument"
 
 testList =  TestList $ map TestCase
     [assertEqual "" (Number 2) (evaluator "(+ 1 1)"),
@@ -504,7 +529,7 @@ testList =  TestList $ map TestCase
      assertEqual "string->list" (List [Char 'A', Char 'b', Char 'c']) (evaluator "(string->list \"Abc\")"),
      assertEqual "list->string" (String "Cmsk") (evaluator "(list->string '(#\\C #\\m #\\s #\\k))"),
      -- Implement string->immutable-string ?
-     -- string-set!, string-copy, string-copy!, string-fill!, string-append, string->list, list->string, build-string
+     -- string-set!, string-copy, string-copy!, string-fill!, build-string
 
 -- test list primitive functionality
      assertEqual "implement eq?" (Bool True) (evaluator "(eq? 'a 'a)"),
@@ -532,4 +557,3 @@ testList =  TestList $ map TestCase
 
 tests = runTestTT testList
 
-{-list = List [Atom "cond",List [List [Atom ">",Number 3,Number 2],List [Atom "quote",Atom "greater"]],List [List [Atom "<",Number 3,Number 2],List [Atom "quote",Atom "less"]]]-}
