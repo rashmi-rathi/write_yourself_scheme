91100231e52ae988fd0019e59081c234446974d1
Jasbir Singh <jasbirsingh279@gmail.com>
2015-12-29 17:41:33 +0000

    WIP

--- src/main.hs
+++ src/main.hs
@@ -8,6 +8,7 @@ import Data.Array
 import Control.Monad
 import Control.Monad.Error
 import System.IO
+import Data.IORef
 import Text.ParserCombinators.Parsec hiding (spaces)
 import Numeric
 
@@ -489,10 +490,55 @@ until_ pred prompt action = do
 runRepl :: IO ()
 runRepl = until_ (== "quit") (readPrompt "Lisp>> ") evalAndPrint
 
+type Env = IORef [(String, IORef LispVal)]
+nullEnv :: IO Env
+nullEnv = newIORef []
+type IOThrowsError = ErrorT LispError IO
+liftThrows :: ThrowsError a -> IOThrowsError a
+liftThrows (Left err) = throwError err
+liftThrows (Right val) = return val
+runIOThrows :: IOThrowsError String -> IO String
+runIOThrows action = runErrorT (trapError action) >>= return . extractValue
+isBound :: Env -> String -> IO Bool
+isBound envRef var = readIORef envRef >>= return . maybe False (const True) . lookup var
+getVar :: Env -> String -> IOThrowsError LispVal
+getVar envRef var =
+  do
+    env <- liftIO $ readIORef envRef
+    maybe (throwError $ UnboundVar "Getting an ubound variable" var) (liftIO . readIORef) (lookup var env)
+
+setVar :: Env -> String -> LispVal -> IOThrowsError LispVal
+setVar envRef var value =
+  do
+    env <- liftIO $ readIORef envRef
+    maybe (throwError $ UnboundVar "Setting an unbound variable" var) (liftIO . (flip writeIORef value)) (lookup var env)
+    return value
+
+defineVar :: Env -> String -> LispVal -> IOThrowsError LispVal
+defineVar envRef var value =
+  do
+    alreadyDefined <- liftIO $ isBound envRef var
+    if alreadyDefined
+       then setVar envRef var value >> return value
+       else liftIO $ do
+            valueRef <- newIORef value
+            env <- readIORef envRef
+            writeIORef envRef ((var, valueRef) : env)
+            return value
+
+bindVars :: Env -> [(String, LispVal)] -> IO Env
+bindVars envRef bindings = readIORef envRef >>= extendEnv bindings >>= newIORef
+  where
+    extendEnv bindings env = liftM (++ env) (mapM addBinding bindings)
+    addBinding (var, value) =
+      do ref <- newIORef value
+         return (var, ref)
+
 main :: IO ()
 main =
   do
     args <- getArgs
+
     case length args of
       0 -> runRepl
       1 -> evalAndPrint $ args !! 0
