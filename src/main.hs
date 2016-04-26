646447cbf09c7da3b0c45245369f3ce7c68e7529
Jasbir Singh <jasbirsingh279@gmail.com>
2016-04-26 22:34:45 +0100

    Fix function eqv

--- src/main.hs
+++ src/main.hs
@@ -462,6 +462,12 @@ eqv [Bool arg1, Bool arg2] = return $ Bool (arg1 == arg2)
 eqv [Number arg1, Number arg2] = return $ Bool (arg1 == arg2)
 eqv [String arg1, String arg2] = return $ Bool (arg1 == arg2)
 eqv [Atom arg1, Atom arg2] = return $ Bool (arg1 == arg2)
+eqv [(DottedList xs x), (DottedList ys y)] = eqv [List $ xs ++ [x], List $ ys ++ [y]]
+eqv [(List arg1), (List arg2)]             = return $ Bool $ (length arg1 == length arg2) &&
+                                                             (all eqvPair $ zip arg1 arg2)
+     where eqvPair (x1, x2) = case eqv [x1, x2] of
+                                Left err -> False
+                                Right (Bool val) -> val
 {-eqv [List arg1, List arg2] = return $ Bool ((length arg1 == length arg2) && (and $ zipWith (==) arg1 arg2)) --different from Tang's-}
 eqv [_, _] = return $ Bool False
 eqv badArgsList = throwError $ NumArgs 2 badArgsList
